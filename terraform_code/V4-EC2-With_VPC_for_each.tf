ee28467313f635532f3207c929f7ef28a16cfdd7
Ratnadweep <rghosh03311995@gmail.com>
2025-08-13 02:37:41 +0530

    fjhg

--- terraform_code/V4-EC2-With_VPC_for_each.tf
+++ terraform_code/V4-EC2-With_VPC_for_each.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners = [ "099720109477" ]
+  owners      = ["099720109477"]
   filter {
-    name = "name"
-    values = [ "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*" ]
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 }
 
