cfbe53615243f91350d525d8254d688c299c0a10
Ratnadweep <rghosh03311995@gmail.com>
2025-08-17 00:25:14 +0530

    adding port 8000

--- terraform_code/V4-EC2-With_VPC_for_each.tf
+++ terraform_code/V4-EC2-With_VPC_for_each.tf
@@ -45,6 +45,14 @@ resource "aws_security_group" "demo-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    //description = "Container port"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
